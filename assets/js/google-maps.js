607d7024d8e7e9912124fcbf7c9915818722227d
Brock Ericksen <brockericksen@gmail.com>
2023-02-05 00:22:17 -0500

    inti

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -3,7 +3,7 @@ function initMap() {
     var myLatLng = {lat: 32.16, lng: -80.75};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
-        zoom: 10,
+        zoom: 9,
         center: myLatLng
     });
 
